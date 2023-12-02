092aafc63b5449ff82b8a9ad76a96221f3e09a8d
KillerLoM <nguyenminhtam01632.fetel@gmail.com>
2023-12-02 17:28:58 +0700

    git init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    title: 'Trang chủ'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
+ export default routes;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'booking-admin';
+  isLine = false;
+  constructor(){
+    this.isLine = false;
+  }
+  createLine(){
+    this.isLine = true;
+  }
 }
